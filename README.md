d08f995d4666d0f28c66599318154eb498c06548
chobi1125 <chonmage112000@yahoo.co.jp>
2020-12-19 18:57:37 +0900

    styled-components導入まで

---
+++ README.md
@@ -0,0 +1,10 @@
+## YouTubeViewer
+
+js/app.jsを読み込んで動作する
+
+コマンド
+```
+$ npm start // port3000
+$ npm run watch 
+$ npm run storybook // port 6006
+```
---
+++ webpack.config.babel.js
@@ -0,0 +1,30 @@
+import path from 'path';
+
+export default (env, args) => {
+  const isProduction = args.mode === 'production';
+  const devtool = !isProduction && 'inline-source-map';
+  const rules = [
+    {
+      test: /\.jsx?$/,
+      use: ['babel-loader'],
+    },
+  ];
+
+  return {
+    devtool,
+    entry: './src/entries/app.jsx',
+    output: {
+      path: path.join(__dirname, './public/js/'),
+      filename: 'app.js',
+    },
+    module: { rules },
+    resolve: {
+      modules: ['node_modules'],
+      alias: {
+        //チルダはsrc以下から探すのエイリアス
+        '~': path.join(__dirname, './src/'),
+      },
+      extensions: ['.js', '.jsx'],
+    },
+  };
+};
